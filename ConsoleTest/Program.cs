af5b6f68a007ab5f23ea30df282fb48f4e2f49cd
asd <asd@MSI>
2021-07-21 14:38:05 +0300

    Proje dosyası ekle.

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+using DataAccess.Concrete.EntityFramework;
+
+namespace ConsoleTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new DataAccess.Concrete.EntityFramework.EfProductDal());
+            foreach (var product in productManager.GetByUnitPrice (10,200))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+           
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,84 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class EfProductDal : IProductDal
+    {
+        List<Product> _products;
+        public EfProductDal()
+        {
+            _products = new List<Product> {
+            
+            new Product { ProductId = 1, CategoryId = 1, ProductName = "Hat", UnitPrice = 100, UnitsInStock = 20 },
+            new Product { ProductId = 2, CategoryId = 2, ProductName = "Mouse", UnitPrice = 200, UnitsInStock = 12 },
+            new Product { ProductId = 3, CategoryId = 2, ProductName = "Keyboard", UnitPrice = 450, UnitsInStock = 2 },
+            new Product { ProductId = 4, CategoryId = 1, ProductName = "Shirt", UnitPrice = 100, UnitsInStock = 30 }
+
+
+            };
+
+
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete = null;
+            //foreach (var p in _products)
+            //{
+            //    if (product.ProductId == p.ProductId)
+            //    {
+            //        productToDelete = p;
+            //    }
+
+            //}
+            productToDelete = _products.SingleOrDefault(p=>p.ProductId==product.ProductId);
+
+              _products.Remove(productToDelete);
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int CategoryId)
+        {
+            return  _products.Where(p => p.CategoryId == CategoryId).ToList();
+
+
+        }
+
+        public void Update(Product product)
+        {
+
+           Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+
+        }
+    }
+}
