36e6d7f50b2245fc91ee0d2fd49db73ef635381a
cyanthelynn <84150646+cyanthelynn@users.noreply.github.com>
2021-07-30 23:51:19 +0300

    cbn
    agh

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAddedMessage = "Product Added!";
+        public static string ProductAddedErrorMessage = "Something went wrong! Please Try Again.";
+        public static string MaintenanceTime = "System is offline";
+        public static string ProductListedMessage = "Products Listed!";
+    }
+}
--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -9,12 +9,62 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
+            // GetByUnitPriceTest();
+            //CategoryGetAllTest();
+            DtoTest();
+        }
+
+
+        private static void CategoryGetAllTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void GetByUnitPriceTest()
+        {
             ProductManager productManager = new ProductManager(new DataAccess.Concrete.EntityFramework.EfProductDal());
-            foreach (var product in productManager.GetByUnitPrice (10,200))
+            foreach (var product in productManager.GetByUnitPrice(10, 200).Data)
             {
                 Console.WriteLine(product.ProductName);
             }
-           
+
         }
+        private static void DtoTest()
+        {
+            ProductManager productManager1 = new ProductManager(new DataAccess.Concrete.EntityFramework.EfProductDal());
+            var result = productManager1.GetProductDetails();
+
+            if (result.Success == true)
+            {
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + " / " + product.CategoryName + " / " + product.UnitsInStock);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+            
+        }
+
+
+
+
     }
 }
+
+	
+	
+
+	
+
+       
+        
+
+
